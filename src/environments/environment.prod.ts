c2e1d3711a71552ed2ca45e16c6e5013124848f6
Siew Wing Fei <wingfei.siew@nova-hub.com>
2021-11-12 11:24:46 +0800

    j

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'http://nh-callisto:8081/vesalius-his-backend-2.0.1',
-  mykadUrl: 'http://localhost:5000'
+  mykadUrl: 'http://nh-callisto:8080/ngws'
 };
